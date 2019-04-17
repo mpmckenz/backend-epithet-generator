d7c0a4774cf47395b96bf8803402e258242f2416
Michael McKenzie <mikemckenzie987@gmail.com>
2019-04-17 14:45:56 -0400

    environment variables are accessible in app

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, jsonify
+from dotenv import load_dotenv
+
+import os
 
 app = Flask(__name__)
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+flask_app = os.getenv('FLASK_APP')
+flask_env = os.getenv('FLASK_ENV')
 
 @app.route('/')
 def random_epithat():
